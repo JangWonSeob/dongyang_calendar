f2e2a32caad56aab4360a43d9f6d491724a4151d
JangWonSeob <jqk9807@gmail.com>
2022-05-22 18:08:00 +0900

    MongoDB 연동

--- calendar_backend/app.js
+++ calendar_backend/app.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const app = express();
 
-const port = 4000;
+const port = 5000;
 const host = "localhost";
+
+const config = require("./config/mongoDb");
+
+const mongoose = require("mongoose");
+const connect = mongoose
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
 
 app.listen(port, () => {
   console.log(`Hello, Express Server http://${host}:${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.json({ result: true });
 });
